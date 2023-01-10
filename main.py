054ac3c1e32b7670cb1d2734862dfe5c3edfee57
Kristjan Cuznar <kristjancuznar0@gmail.com>
2023-01-10 14:49:36 +0100

    Dokumentacija...

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ __email__ =  "kristjancuznar0@gmail.com"
 __license__ = "GPLv3"
 __maintainer__ = "developer"
 __status__ = "Production"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 import argparse
 import sys
